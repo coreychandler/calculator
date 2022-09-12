4f2d20a09f48721605a2a87aeadb6042b8695d2e
Corey <corey.chandler4@gmail.com>
2022-09-11 21:23:29 -0400

    implement keyboard support

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A simple web-based calculator with keyboard support. Logic could be cleaned up and needs key input support, will work on this next
+A simple web-based calculator with keyboard support.
+
+KNOWN BUGS/GAPS: 
+can input too many numbers on the screen
+clear and delete need key presses implemented
--- app.js
+++ app.js
@@ -24,6 +24,7 @@ let clearNext = false;
 let formulaValue;
 let operatorWasPressed = false;
 let validKeys = [];
+let nonNumberKeys = ["*", "-", "+", "/"];
 
 function handleLoad(e) {
     // userInput.textContent = "0"; //this is a nice to have
@@ -51,22 +52,58 @@ function handleButtonClick(e) {
 
 
 function handleKeydown(e) {
-    if (!validKeys.includes(e.key)){
-        return;
-    } 
-    if (!operatorWasPressed) {
-        firstOperand += e.key;
+    let currentButton;
+    if (nonNumberKeys.includes(e.key)) {
+        currentButton = document.querySelector(`button[data-operator="${e.key}"]`);
+    } else if (e.key === "Enter" || e.key === "=") {
+        currentButton = document.querySelector("#equals");
+    } else if (e.key === "."){
+        currentButton = document.querySelector("#dot");
     } else {
-
-        secondOperand += e.key;
+        currentButton = document.querySelector(`button[data-number="${e.key}"]`);
     }
-    userInput.textContent += e.key;
+    currentButton.click();
+    currentButton.classList.toggle("keyclick");
+    window.setTimeout(() => {
+        currentButton.classList.toggle("keyclick");
+    }, 100);
 
+
+    // if (e.key === "Enter") {
+    //     handleEqualsButton(e);
+    // }
+
+    // if (!validKeys.includes(e.key)) {
+    //     return;
+    // } 
+    // if (e.key === "*" || e.key === "-" || e.key === "/" || e.key === "+") {
+    //     handleOperatorKeyDown(e);
+    //     clearNext = true;
+    //     return;
+    // } else if (e.key === ".") {
+    //     handleDotButton(e);
+    //     return;
+    // } else if (e.key === "Enter") {
+    //     handleEqualsButton(e);
+    // } else {
+    // if (!operatorWasPressed) {
+    //     firstOperand += e.key;
+    //     clearNext = false;
+    // } else {
+    //     enteredSecondValue = true;
+    //     if (secondOperand == undefined) {
+    //         userInput.textContent = "";
+    //     }
+    //     secondOperand += e.key;
+    //     clearNext = false;
+    // }
+    // userInput.textContent += e.key;
+    // }
 }
 
 
 function handleOperatorClick(e) {
-    if (enteredSecondValue ) {
+    if (enteredSecondValue) {
         formula.textContent = operate(currentOperator, firstOperand, secondOperand) + " " + e.target.innerText;
         handleEqualsButton(e);
     }
@@ -79,35 +116,51 @@ function handleOperatorClick(e) {
     clearNext = true;
 }
 
+function handleOperatorKeyDown(e) {
+    if (enteredSecondValue) {
+        formula.textContent = operate(currentOperator, firstOperand, secondOperand) + " " + e.key;
+        handleEqualsButton(e);
+    }
+    operatorWasPressed = true;
+    currentOperator = e.key;
+    firstOperand = Number(userInput.textContent);
+    // formula.textContent = `${firstOperand} ${e.key}`;
+    formula.textContent = firstOperand + " " + e.key;
+    formula.textContent = formula.textContent.replace("*", "x");
+    userInput.textContent = firstOperand;
+    clearNext = true;
+}
+
 function handleClearButton(e) {
     firstOperand = "";
     secondOperand = "";
     userInput.textContent = "";
     formula.textContent = "";
+    clearNext = false;
 }
 
 function handleDotButton(e) {
-    if(userInput.textContent.includes(".")) {return};
+    if (userInput.textContent.includes(".") || userInput.textContent === "") { return };
 
     userInput.textContent += ".";
 }
 
 function handleDeleteButton(e) {
-    userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length -1);
+    userInput.textContent = userInput.textContent.substring(0, userInput.textContent.length - 1);
 }
 
 function handleEqualsButton(e) {
-    if (!enteredSecondValue || firstOperand === "") return;
+    if (!enteredSecondValue || firstOperand === "") return; //handle if equals is pushed too early or if nothing there
     secondOperand = Number(userInput.textContent);
     userInput.textContent = operate(currentOperator, firstOperand, secondOperand);
-    // formula.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`;
-    formula.textContent = firstOperand + " " + currentOperator + " " + secondOperand + " =";
-    if (userInput.textContent === "Infinity"){
+    formula.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`;
+    // formula.textContent = firstOperand + " " + currentOperator + " " + secondOperand + " =";
+    if (userInput.textContent === "Infinity") {
         userInput.textContent = "";
         firstOperand = "";
         secondOperand = "";
         formula.textContent = "";
-    } 
+    }
     operatorWasPressed = false;
     enteredSecondValue = false;
 }
@@ -133,6 +186,7 @@ function buildValidKeyArray() {
         validKeys.push(operatorButtons[i].innerText);
     }
     validKeys[validKeys.indexOf("x")] = "*";
+    validKeys.push(".");
 }
 
 function add(a, b) {
@@ -154,6 +208,8 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
     switch (operator) {
         case "x":
+            return +multiply(num1, num2).toFixed(3);
+        case "*":
             return +multiply(num1, num2).toFixed(3);
         case "/":
             return +divide(num1, num2).toFixed(3);
